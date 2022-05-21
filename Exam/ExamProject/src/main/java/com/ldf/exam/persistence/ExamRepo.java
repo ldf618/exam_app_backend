fadf489dc3e718e2aa01f5b02ecdaa4b9047a24c
ldiez <ldiez@LAPTOP-99ECVK8O>
2022-05-21 16:51:00 +0100

    Merge origin/main into main
    Conflicts:
    	hibernate07/src/main/java/util/JPASessionUtil.java

--- Exam/ExamProject/src/main/java/com/ldf/exam/persistence/ExamRepo.java
+++ Exam/ExamProject/src/main/java/com/ldf/exam/persistence/ExamRepo.java
@@ -10,6 +10,8 @@ import com.ldf.exam.model.ExamQuestion;
 import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 /**
@@ -20,6 +22,13 @@ public interface ExamRepo extends JpaRepository<Exam, Integer> {
     
     public List<Exam> findByCourse(int idCourse);
     public List<Exam> findByPublicationDateIsNotNullAndPublicationDateBetween(LocalDate startDate, LocalDate endDate);
-    //public List<Exam> findByQuestionTypeAndCourse(Integer idCourse, ExamQuestion.QuestionType questionType1, ExamQuestion.QuestionType questionType2);
+    
+    @Query("select distinct e from Exam e inner join e.examQuestions eq "
+            + "where e.publicationDate is not null "
+            + "and e.course.id=:idCourse and eq.type in (:questionType1,:questionType2)")
+    public List<Exam> findByQuestionTypeAndCourse(
+            @Param("idCourse") Integer idCourse,
+            @Param("questionType1") ExamQuestion.QuestionType questionType1, 
+            @Param("questionType2") ExamQuestion.QuestionType questionType2);
         
 }
